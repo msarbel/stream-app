e2dc2de9b0a4887f97f1ea40ec06f432818a698f
Msarbel <35122452+msarbel@users.noreply.github.com>
2019-02-14 19:04:17 -0500

    Added RTMP server & OBS Live streaming capability

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -15,7 +15,7 @@ class StreamList extends React.Component {
                     <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
                         Edit
                     </Link>
-                    <Link to="/" className="ui button negative">
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
                         Delete
                     </Link>
                 </div>
@@ -30,7 +30,9 @@ class StreamList extends React.Component {
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`} className="header"> 
+                            {stream.title}
+                        </Link>
                         <div className="description">{stream.description}</div>
                     </div>
                 </div>
